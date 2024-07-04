9b972e3e3d10a035dc2b28e4dcdbbf6b8fe608e4
chris-hndz <chris-hndz@users.noreply.github.com>
2024-07-04 02:07:41 +0200

    Update SMPLXAligner.cs

--- Unity_Scripts/SMPLXAligner.cs
+++ Unity_Scripts/SMPLXAligner.cs
@@ -10,12 +10,13 @@ public class SMPLXAligner : MonoBehaviour
     public Texture2D backgroundImage;
     public string jsonFilePath;
 
-    public Vector3 manualOffset = new Vector3(0f, -1f, 0f); // Ajusta estos valores según sea necesario
+    public Vector3 manualOffset = new Vector3(0.08f, -1.55f, 0.16f); // Ajusta estos valores según sea necesario
     private GameObject instanciaSmplx;
 
 
     private List<GameObject> smplxInstances = new List<GameObject>();
-    private string[] _smplxJointNames = new string[] { "pelvis","right_hip","left_hip","spine1","right_knee","left_knee","spine2","right_ankle","left_ankle","spine3", "right_foot","left_foot","neck","right_collar","left_collar","head","right_shoulder","left_shoulder","right_elbow","left_elbow", "right_wrist","left_wrist","right_index1","right_index2","right_index3","right_middle1","right_middle2","right_middle3","right_pinky1","right_pinky2","right_pinky3","right_ring1","right_ring2","left_ring3","right_thumb1","right_thumb2","right_thumb3","left_index1","left_index2","left_index3","left_middle1","left_middle2","left_middle3","left_pinky1","left_pinky2","left_pinky3","left_ring1","left_ring2","right_ring3","left_thumb1","left_thumb2","left_thumb3", "jaw"};
+    //private string[] _smplxJointNames = new string[] { "pelvis","right_hip","left_hip","spine1","right_knee","left_knee","spine2","right_ankle","left_ankle","spine3", "right_foot","left_foot","neck","right_collar","left_collar","head","right_shoulder","left_shoulder","right_elbow","left_elbow", "right_wrist","left_wrist","right_index1","right_index2","right_index3","right_middle1","right_middle2","right_middle3","right_pinky1","right_pinky2","right_pinky3","right_ring1","right_ring2","left_ring3","right_thumb1","right_thumb2","right_thumb3","left_index1","left_index2","left_index3","left_middle1","left_middle2","left_middle3","left_pinky1","left_pinky2","left_pinky3","left_ring1","left_ring2","right_ring3","left_thumb1","left_thumb2","left_thumb3", "jaw"};
+    private string[] _smplxJointNames = new string[] { "pelvis", "left_hip", "right_hip", "spine1", "left_knee", "right_knee", "spine2", "left_ankle", "right_ankle", "spine3", "left_foot", "right_foot", "neck", "left_collar", "right_collar", "head", "left_shoulder", "right_shoulder", "left_elbow", "right_elbow", "left_wrist", "right_wrist", "left_index1", "left_index2", "left_index3", "left_middle1", "left_middle2", "left_middle3", "left_pinky1", "left_pinky2", "left_pinky3", "left_ring1", "left_ring2", "left_ring3", "left_thumb1", "left_thumb2", "left_thumb3", "right_index1", "right_index2", "right_index3", "right_middle1", "right_middle2", "right_middle3", "right_pinky1", "right_pinky2", "right_pinky3", "right_ring1", "right_ring2", "right_ring3", "right_thumb1", "right_thumb2", "right_thumb3", "jaw"};
     private Vector3 initialPosition;
     private bool canBeUpdated = false;
 
@@ -103,16 +104,14 @@ public class SMPLXAligner : MonoBehaviour
         SMPLX smplxComponent = smplxInstance.GetComponent<SMPLX>();
         if (smplxComponent != null)
         {
-            //smplxComponent.SetPose(parames.pose);
-            //smplxComponent.SetShape(parames.shape);
-            //smplxComponent.SetExpression(parames.expression);
-
             //Set Betas
             if (parames.shape != null) SetShape(smplxComponent, parames.shape);
 
             //Set Pose
             if (parames.pose != null) SetPose(smplxComponent, parames.pose);
-            else Debug.Log("parames.pose es null!");
+
+            //Set Expression
+            if (parames.expression != null) SetExpression(smplxComponent, parames.expression);
         }
     }
 
@@ -127,8 +126,9 @@ public class SMPLXAligner : MonoBehaviour
         
         // Ajustar la rotación
         Quaternion worldRotation = Quaternion.Euler(parames.rotationV.x, 
-                                                    parames.rotationV.y + 180f, 
-                                                    -parames.rotationV.z);
+                                                    -parames.rotationV.y /*+ 180f*/, 
+                                                    -parames.rotationV.z + 180f);
+        //Quaternion worldRotation = Quaternion.Euler(parames.rotationV * Mathf.Rad2Deg);
 
         // Aplicar rotación y posición
         smplxInstance.transform.rotation = worldRotation;
@@ -141,6 +141,12 @@ public class SMPLXAligner : MonoBehaviour
 
     private void SetShape(SMPLX smplxMannequin, float[] newBetas)
     {
+        if (newBetas.Length != SMPLX.NUM_BETAS)
+        {
+            Debug.LogError("No tiene el número correcto de betas: " + newBetas.Length + " porque deberían ser: " + SMPLX.NUM_BETAS);
+            return;
+        }
+        
         for (int i = 0; i < SMPLX.NUM_BETAS; i++)
         {
             smplxMannequin.betas[i] = newBetas[i];
@@ -148,25 +154,64 @@ public class SMPLXAligner : MonoBehaviour
         }
         smplxMannequin.SetBetaShapes();
 
-        Debug.Log("Se han aplicado correctamente los blendshapes");
+        Debug.Log("Se han aplicado correctamente los blendshapes.");
     }
 
     private void SetPose(SMPLX smplxMannequin, float[][] newPose) 
     {
-        Debug.Log("El largo de las poses es de: " + newPose.Length);
-        
-        /*
-        for (int i = 0; i < newPose.Length; i++)
-            Debug.Log("newPose[" + i + "][0]: " + newPose[i][0] + " newPose[" + i + "][1]: " + newPose[i][1] + " newPose[" + i + "][2]: " + newPose[i][2]);
-        */
+        //Debug.Log("El largo de las poses es de: " + newPose.Length);
 
         if (newPose.Length != _smplxJointNames.Length)
         {
-            Debug.LogError("no tiene el numero correcto de valores " + newPose.Length);
+            Debug.LogError("No tiene el número correcto de valores: " + newPose.Length + " porque deberían ser: " + _smplxJointNames.Length);
             return;
         }
 
-        return;   
+        for (int i = 0; i < newPose.Length; i++)
+        {
+            string jointName = _smplxJointNames[i];
+            float rodX = newPose[i][0];
+            float rodY = newPose[i][1];
+            float rodZ = newPose[i][2];
+
+            // Convertir de notación de Rodrigues a cuaternión
+            Quaternion rotation = SMPLX.QuatFromRodrigues(rodX, rodY, rodZ);
+
+            // Aplicar la rotación al joint
+            smplxMannequin.SetLocalJointRotation(jointName, rotation);
+        }
+
+        // Actualizar las posiciones de los joints y los correctivos de pose
+        smplxMannequin.UpdateJointPositions();
+        smplxMannequin.UpdatePoseCorrectives();
+
+        Debug.Log("Se ha aplicado correctamente la pose.");
+    }
+
+    /*private Quaternion QuatFromRodrigues(float rodX, float rodY, float rodZ)
+    {
+        Vector3 axis = new Vector3(-rodX, rodY, rodZ);
+        float angle_deg = -axis.magnitude * Mathf.Rad2Deg;
+        axis.Normalize();
+
+        return Quaternion.AngleAxis(angle_deg, axis);
+    }*/
+
+    private void SetExpression(SMPLX smplxMannequin, float[] newExpression) 
+    {
+        if (newExpression.Length != SMPLX.NUM_EXPRESSIONS)
+        {
+            Debug.LogError("No tiene el número correcto de expresiones: " + newExpression.Length + " porque deberían ser: " + SMPLX.NUM_EXPRESSIONS);
+            return;
+        }
         
+        for (int i = 0; i < SMPLX.NUM_EXPRESSIONS; i++)
+        {
+            smplxMannequin.expressions[i] = newExpression[i];
+            Debug.Log("newExpression[" + i + "]: " + newExpression[i]);
+        }
+        smplxMannequin.SetExpressions();
+
+        Debug.Log("Se han aplicado correctamente las expresiones.");
     }
 }
